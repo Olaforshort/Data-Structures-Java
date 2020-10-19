60376dbd12ba95b354577b731dfb8e66f1790b1b
Giovanni Lituma <giovanninet@icloud.com>
2020-10-19 15:50:48 -0400

    implement addFirst and addLast methods

--- src/dev/giovannix/LinkedList.java
+++ src/dev/giovannix/LinkedList.java
@@ -14,4 +14,40 @@ public class LinkedList {
             this.value = value;
         }
     }
+
+    public void addLast(int item) {
+        Node node = new Node(item);
+        if (isEmpty())
+            first = last = node;
+        else {
+            last.next = node;
+            last = node;
+        }
+    }
+
+    //    public void addFirst(int item) {
+    //        Node node = new Node(item);
+    //        if (first == null) {
+    //            first = last = node;
+    //        }
+    //        else {
+    //            first = first.next;
+    //            first = node;
+    //        }
+    //    }
+
+    public void addFirst(int item) {
+        Node node = new Node(item);
+
+        if (isEmpty()) {
+            first = last = node;
+        } else {
+            node.next = first;
+            first = node;
+        }
+    }
+
+    private boolean isEmpty() {
+        return (first == null);
+    }
 }
--- src/dev/giovannix/Main.java
+++ src/dev/giovannix/Main.java
@@ -3,11 +3,14 @@ package dev.giovannix;
 public class Main {
 
     public static void main(String[] args) {
-        java.util.LinkedList<Integer> x = new java.util.LinkedList<>();
-        x.addFirst(5);
-        x.addLast(10);
+//        java.util.LinkedList<Integer> x = new java.util.LinkedList<>();
+//        x.addFirst(5);
+//        x.addLast(10);
 
         var list = new LinkedList();
-
+        list.addFirst(10);
+        list.addLast(15);
+        list.addFirst(5);
+        list.addLast(20);
     }
 }
